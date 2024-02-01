88a850be98e3adb410d5e2afabb7b9d2d713c941
Dracks <jaume.singlavalls@gmail.com>
2024-02-01 19:04:40 +0000

    Deploying to gh-pages from @ Dracks/Codename-Recon@d6487b567a6006f3828b6fe18224af72e701037b 🚀

---
+++ coi-serviceworker.js
@@ -0,0 +1,11 @@
+--2024-02-01 19:04:33--  https://raw.githubusercontent.com/gzuidhof/coi-serviceworker/master/coi-serviceworker.min.js
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.111.133, 185.199.108.133, 185.199.110.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.111.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3009 (2.9K) [text/plain]
+Saving to: 'coi-serviceworker.min.js'
+
+     0K ..                                                    100% 67.2M=0s
+
+2024-02-01 19:04:33 (67.2 MB/s) - 'coi-serviceworker.min.js' saved [3009/3009]
+
